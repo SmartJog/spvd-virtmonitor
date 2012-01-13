c33cf55571d9aeb9c611093ccf281aa17c0ee90d
Gilles Dartiguelongue <gilles.dartiguelongue@smartjog.com>
2012-01-13 15:38:31 +0100

    Rewrite get_status check to collect services status as well, closes: #5465

--- virtmonitor.py
+++ virtmonitor.py
@@ -86,13 +86,24 @@ class Job(BaseJob):
             imp['timeout'] = self.params['importer_tcp_timeout']
 
         try:
-            status = imp.call('virtmanager.services', 'get_status')
-            offline_vms = sum([vm for vm in status if vm[status] == 'offline'])
+            vms_status = imp.call('virtmanager.services', 'get_status')
+            svc_status = imp.call('spv.services', 'get_status', {
+                'plugin_name' : 'virtmonitor',
+                'plugin_check': 'get_service_status',
+            })
 
-            if offline_vms > 0:
-                return 'ERROR', '%s over %s VMs currently offline' % (offline_vms, len(status))
+            offline_vms = sum([vm_name for vm_name, vm_status in vms_status.iteritems()
+                if vm_status == 'offline'])
+            offline_svc = sum([1 for _, svc in svc_status['status'].iteritems()
+                if svc['check_status'] != 'FINISHED'])
+
+            if offline_vms > 0 or offline_svc > 0:
+                return 'ERROR', '%s of %s VMs currently offline plus %s of %s VMs services down' % (
+                    offline_vms, len(vms_status),
+                    offline_svc, len(svc_status['status']),
+                )
             else:
-                return 'FINISHED', 'All VMs currently online'
+                return 'FINISHED', 'All VMs and services currently online'
         except ImporterError, exc:
             raise Job.BaseError('Importer error, please check remote end')
 
